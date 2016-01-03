2d167032ea9599c4a6178cded427b7317cb338b5
chiggs <chris.higgs@potentialventures.com>
2016-01-03 17:18:15 +0000

    Update build script to run do_sim.sh

--- travis/build.sh
+++ travis/build.sh
@@ -5,11 +5,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Synthesis build
 if [ "${QUARTUS_SYNTHESIS}" = "true" ]; then
     ${DIR}/do_quartus.sh
-fi
-
-# Simulation build
-if [ "${COCOTB_SIMULATION}" = "true" ]; then
-    git clone https://github.com/potentialventures/cocotb.git
-    export COCOTB=$PWD/cocotb
-    cd tb && make
+else
+    ${DIR}/do_sim.sh
 fi
