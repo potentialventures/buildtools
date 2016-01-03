952c6dcfe719c49395abf796a8c4598905cd2263
chiggs <chris.higgs@potentialventures.com>
2016-01-03 19:11:48 +0000

    Correct syntax error in bash script

--- travis/do_sim.sh
+++ travis/do_sim.sh
@@ -11,7 +11,7 @@ tar -zxf sim.tar.gz
 export PATH=`pwd`/sim/bin:$PATH
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ "${VUNIT_FUSESOC_SIMULATION}" = "true"]; then
+if [ "${VUNIT_FUSESOC_SIMULATION}" = "true" ]; then
     ${DIR}/../funit/funit.py -x results.xml
 fi
 
@@ -22,3 +22,4 @@ fi
 # Generate HMAC digest and post the results back to The Open Corps
 MSG_DIGEST=`openssl dgst -hex -sha1 -hmac $MSG_TOKEN results.xml | awk '{print $2}'`
 curl -H "X-Core-Signature: ${MSG_DIGEST}" -H "Content-Type:application/xml" --data-binary @results.xml https://theopencorps.potential.ventures/${REPOSITORY}/simulation/results
+
