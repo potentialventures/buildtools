45bbd6d6fbc99a71b2b91df2c637977004e9b35b
chiggs <chris.higgs@potentialventures.com>
2016-01-17 18:49:24 +0000

    Add support for iterating over VUnit scripts and combining the results

--- travis/do_sim.sh
+++ travis/do_sim.sh
@@ -15,14 +15,24 @@ if [ "${VUNIT_FUSESOC_SIMULATION}" = "true" ]; then
     ${DIR}/../funit/funit.py -x results.xml --exit-0 | tee simulation.log
 fi
 
+if [ "${VUNIT_SIMULATION}" = "true" ]; then
+    for vunit_script in ${VUNIT_SCRIPTFILES}; do
+        cd `dirname $vunit_script` && $vunit_script -x results.xml --exit-0 | tee -a ~/simulation.log
+    done
+fi
+
+
 if [ "${COCOTB_SIMULATION}" = "true" ]; then
     make
 fi
 
+cd
+${DIR}/../junit/combine.py combined_results.xml
+
 # Generate HMAC digest and post the results back to The Open Corps
 # NB Let's Encrypt CA isn't in the curl bundle on Trusty
-MSG_DIGEST=`openssl dgst -hex -sha1 -hmac $MSG_TOKEN results.xml | awk '{print $2}'`
-curl -k -H "X-Hub-Signature:sha1=${MSG_DIGEST}" -H "Content-Type:application/xml" -H "Travis-Commit:${TRAVIS_COMMIT}" -H "Travis-JobID:${TRAVIS_JOB_ID}" -H "Travis-BuildID:${TRAVIS_BUILD_ID}" --data-binary @results.xml https://theopencorps.potential.ventures/${REPOSITORY}/simulation/results
+MSG_DIGEST=`openssl dgst -hex -sha1 -hmac $MSG_TOKEN combined_results.xml | awk '{print $2}'`
+curl -k -H "X-Hub-Signature:sha1=${MSG_DIGEST}" -H "Content-Type:application/xml" -H "Travis-Commit:${TRAVIS_COMMIT}" -H "Travis-JobID:${TRAVIS_JOB_ID}" -H "Travis-BuildID:${TRAVIS_BUILD_ID}" --data-binary @combined_results.xml https://theopencorps.potential.ventures/${REPOSITORY}/simulation/results
 
 MSG_DIGEST=`openssl dgst -hex -sha1 -hmac $MSG_TOKEN simulation.log | awk '{print $2}'`
 curl -k -H "X-Hub-Signature:sha1=${MSG_DIGEST}" -H "Content-Type:application/xml" -H "Travis-Commit:${TRAVIS_COMMIT}" -H "Travis-JobID:${TRAVIS_JOB_ID}" -H "Travis-BuildID:${TRAVIS_BUILD_ID}" -H "Content-Filename:simulation.log" --data-binary @simulation.log https://theopencorps.potential.ventures/${REPOSITORY}/simulation/log
