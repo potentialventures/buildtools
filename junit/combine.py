c2a46f0e6f4f207bb6ed30c2ee4d9c681db8db09
Chiggs <chris.higgs@potentialventures.com>
2016-01-17 23:18:07 +0000

    Ensure we populate testsuite level attributes

--- junit/combine.py
+++ junit/combine.py
@@ -5,6 +5,7 @@ Simple script to combine multiple JUnit test results into a single XML file.
 
 import os
 import sys
+from collections import defaultdict
 from xml.etree import cElementTree as ET
 
 
@@ -16,20 +17,27 @@ def find_all(name, path):
 
 def main(path, output, existing="results.xml"):
     rc = 0
-    testsuite = ET.Element("testsuite", name="all", package="all", tests="0")
+    results = defaultdict(int)
+    testsuite = ET.Element("testsuite", name="all", package="all")
 
     for fname in find_all(existing, path):
         tree = ET.parse(fname)
         for element in tree.getiterator("testcase"):
             testsuite.append(element)
+            results["tests"] += 1
 
             for child in element:
-                if child.tag in ["failure", "error"]:
-                    sys.stderr.write("FAILURE: %s.%s\n" %
-                                     (element.attrib["classname"],
-                                      element.attrib["name"]))
-                    rc = 1
-
+                if child.tag == "failure":
+                    results["failures"] += 1
+                    break
+                elif child.tag == "error":
+                    results["errors"] += 1
+                    break
+                elif child.tag == "skipped":
+                    results["skipped"] += 1
+                    break
+    for outcome, count in results.iteritems():
+        testsuite.set(outcome, "%d" % count)
     result = ET.Element("testsuites", name="results")
     result.append(testsuite)
 
